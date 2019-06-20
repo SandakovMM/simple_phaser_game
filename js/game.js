9f81ba432bc834e68d723e1ff438f4503a089bd9
Michail Sandakov <michail.sandakov@eltex.loc>
2019-06-20 14:54:16 +0700

    Add moving for player.

--- js/game.js
+++ js/game.js
@@ -1,9 +1,14 @@
 let gameScene = new Phaser.Scene('Game');
 
+gameScene.init = function() {
+    this.playerSpeed = 2;
+}
+
 gameScene.preload = function() {
     // load images
     this.load.image('background', 'assets/background.png');
-    this.load.image('player', 'assets/player.png');
+    this.load.image('player',     'assets/player.png');
+    this.load.image('enemy',      'assets/dragon.png');
 };
 
 gameScene.create = function() {
@@ -11,19 +16,26 @@ gameScene.create = function() {
     let bg = this.add.sprite(0, 0, 'background');
    
     // change the origin to the top-left corner
-    //bg.setOrigin(0,0);
+    bg.setOrigin(0,0);
    
-    // place sprite in the center
-    bg.setPosition(640/2, 360/2);
+    // create the player
+    this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
+    // we are reducing the width by 50%, and we are doubling the height
+    this.player.setScale(0.5);
    
-    let gameW = this.sys.game.config.width;
-    let gameH = this.sys.game.config.height;
-   
-    console.log(gameW, gameH);
-   
-    console.log(bg);
-    console.log(this);
+    // create an enemy
+    // this.enemy1 = this.add.sprite(250, 180, 'enemy');
+    
+    // this.enemy1.angle = -45;
 };
+
+gameScene.update = function() {
+    // this.enemy1.angle += 1;
+    if(this.input.activePointer.isDown) {
+        // player walks
+        this.player.x += this.playerSpeed;
+    }
+}
 
 let config = {
     type: Phaser.AUTO,
